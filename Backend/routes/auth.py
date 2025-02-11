b08c25af8080237e455d9bac92d22aae37502a40
Oliwia Bandola <oliwia.bandola@gmail.com>
2025-02-11 18:35:23 +0100

    refactor: use the factory pattern and flask blueprints, move routes to separate files

---
+++ Backend/routes/auth.py
@@ -0,0 +1,55 @@
+from flask import request, jsonify, Blueprint
+from flask_jwt_extended import create_access_token, jwt_required
+from extensions import db
+from models import User
+
+
+auth_bp = Blueprint('auth', __name__)
+
+
+@auth_bp.route('/api/register', methods=['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    # Check if username is taken
+    if User.query.filter_by(username=username).first():
+        return jsonify({'error': 'Username is already taken'}), 400
+
+    # Check password requirements
+    if len(password) < 8 or ' ' in password:
+        return jsonify({'error': 'Password should be at least 8 characters long and cannot contain spaces'}), 400
+
+    # Create new user
+    new_user = User(username=username)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'}), 201
+
+
+@auth_bp.route('/api/login', methods=['POST'])
+def login():
+    """Authenticate user and return JWT."""
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    # Check if the user exists
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'error': 'User not found'}), 401
+
+    # Check the password
+    if user.check_password(password):
+        # Generate JWT
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token)
+
+    else:
+        return jsonify({'error': 'Wrong password'}), 401
+
+
+@auth_bp.route('/api/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    pass
---
+++ Backend/routes/ingredients.py
@@ -0,0 +1,49 @@
+from flask import request, jsonify, Blueprint
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from models import Ingredient, User
+from extensions import db
+
+
+ingredients_bp = Blueprint('ingredients', __name__)
+
+
+@ingredients_bp.route('/api/ingredients')
+@jwt_required()
+def get_ingredients():
+    user = User.query.filter_by(username=get_jwt_identity()).first()
+    # if not user:
+    #     return jsonify({'error': 'User not found'}), 404
+
+    ingredients = [ingredient.name for ingredient in user.has_ingredients]
+    return jsonify(ingredients)
+
+
+@ingredients_bp.route('/api/ingredients', methods=['POST'])
+@jwt_required()
+def add_ingredients():
+    user = User.query.filter_by(username=get_jwt_identity()).first()
+    ingredient_name = request.json.get('name').strip()
+    ingredient = Ingredient.query.filter_by(name=ingredient_name).first()
+
+    # Check if user-ingredient relation already exists
+    if ingredient in user.has_ingredients:
+        return ({'error': 'Ingredient already added'}), 400
+
+    # Check if ingredient already exists in the database
+    if not ingredient:
+        # TODO: Function checking and preparing the ingredientformat before adding it to the databse
+        # Add a new ingredient to the database
+        ingredient = Ingredient(name=ingredient_name)
+        db.session.add(ingredient)
+
+    # Add user-ingredient relation
+    user.has_ingredients.append(ingredient)
+    db.session.commit()
+
+    return jsonify({'message': 'Ingredient added successfully', 'ingredient': ingredient.name}), 201
+
+
+@ingredients_bp.route('/api/ingredients/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_ingredients(id):
+    pass
